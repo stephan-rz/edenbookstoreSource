d6f78ea98c67f09c25fa91f56f2728d49e08b824
Shamal Chathuranga <shamalchathuranga2@gmail.com>
2024-05-02 08:12:24 +0530

    config updated

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","edenbookstore");
+$con = mysqli_connect("localhost","root","","ecomstore");
 
 if(!$con){
     die("Connection Failed" . mysqli_connect_error());
